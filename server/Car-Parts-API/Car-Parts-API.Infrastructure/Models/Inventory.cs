f4556c06eedc8a9831d2dd254ee1c3f21ca5d6ce
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:41:03 +0200

    Inventory table.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Inventory.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Inventory.cs
@@ -4,5 +4,18 @@
     {
         //This table would store detailed information about the current inventory of parts, including the
         //quantity on hand, reorder level, and the supplier details.
+        //Ask chatgpt Inventory and Vendor
+
+        public int InventoryId { get; set; }
+
+        public int PartId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime DeliveryDate { get; set; }
+
+        public string SupplierDetails { get; set; }
     }
 }
