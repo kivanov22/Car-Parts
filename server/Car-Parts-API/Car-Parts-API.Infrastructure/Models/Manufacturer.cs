a0e8468ae607f73ff1ac6817b13612dcf5c8e1b4
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:46:45 +0200

    Deleted model

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Manufacturer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Car_Parts_API.Infrastructure.Models
-{
-    public class Manufacturer
-    {
-        //This table would contain detailed information about the manufacturers of the car parts in the database, such as name, location, contact information, and website.
-    }
-}
