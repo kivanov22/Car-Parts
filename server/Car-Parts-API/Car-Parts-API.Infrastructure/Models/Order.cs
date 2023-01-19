72a2e0b7d3f09210dffb407506053b75ff1e7f7e
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:40:35 +0200

    Order table.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Order.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Order.cs
@@ -3,6 +3,27 @@
     public class Order
     {
 
-        //This table would store detailed information about customer orders, including customer information, order date, shipping address, and the specific parts that were ordered along with their quantity.
+        //This table would store detailed information about customer orders, including customer information, order date,
+        //shipping address, and the specific parts that were ordered along with their quantity.
+
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int PartId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal TotalCost { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public string ShippingAddress { get; set; }
+
+        public string Status { get; set; }
+
+        public string Notes { get; set; }
     }
 }
