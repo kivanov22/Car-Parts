a50bf0c6e124f38687219d8b72096560d188e3e2
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:38:13 +0200

    Price table about a part.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Price.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Price.cs
@@ -6,12 +6,24 @@
         // currency.
         public int Id { get; set; }
 
-        public int Part_Number { get; set; }
+        public int PartId { get; set; }
+
+        public decimal PartPrice { get; set; }
+
+        public decimal Discount { get; set; }
+
+        public string Currency { get; set; }
+
+        public string Availability { get; set; }
+
+        public decimal ShippingCost { get; set; }
+
+        public int PartNumber { get; set; }
 
         public string Part_Name { get; set; }
 
         public string ManufacturerName { get; set; }
 
-
+        public string Notes { get; set; }
     }
 }
