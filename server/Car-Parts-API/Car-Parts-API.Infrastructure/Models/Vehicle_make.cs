c1d7f1ccf958d7768446360a0642d8e890551546
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:45:39 +0200

    Vehicle make.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Vehicle_make.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Vehicle_make.cs
@@ -1,7 +1,14 @@
-﻿namespace Car_Parts_API.Infrastructure.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Car_Parts_API.Infrastructure.Models
 {
     public class Vehicle_make
     {
         //"Make ID" - a foreign key that corresponds to the primary key of a "make" table, linking a specific model to a specific make of car
+        [Key]
+        public int MakeId { get; set; }
+
+        public required string MakeName { get; set; }
+
     }
 }
