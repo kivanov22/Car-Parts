461a35f911e5327941f7485fbf2f1346c7f6829c
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:41:32 +0200

    Customer table.

---
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Parts_API.Infrastructure.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public int OrderId { get; set; }
+
+        public Order Orders { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
