9971482046dc426334190947cf213a41efd0b353
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:35:58 +0200

    Compatibiliy table linking a part and model if it is for this model.

---
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Compatibility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Parts_API.Infrastructure.Models
+{
+    public class Compatibility
+    {
+        public int PartId { get; set; }
+
+        public int ModelId { get; set; }
+    }
+}
