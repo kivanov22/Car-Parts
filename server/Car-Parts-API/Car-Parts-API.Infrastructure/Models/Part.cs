36b043935f92a4919e88489f727843ac1518614b
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:39:47 +0200

    Parts table with information about part.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Part.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Part.cs
@@ -4,10 +4,19 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
+        public int PartTypeId { get; set; }
+
+        public Part_Types Part_Types { get; set; }
         public required string PartName { get; set; }
 
         public int PartNumber { get; set; }
 
         public string PartDescription { get; set; }
+
+        public string PartBrand { get; set; }
+
+        //public int Warranty { get; set; }
+
+        //public string Notes { get; set; }
     }
 }
