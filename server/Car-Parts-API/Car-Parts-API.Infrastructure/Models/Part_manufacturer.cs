79f6f0dc046b6f269acb65f014db7e431e9bcb11
Kristian Ivanov <kriskopk@abv.bg>
2023-01-19 16:44:45 +0200

    Table for part manufacturer.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Part_manufacturer.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Part_manufacturer.cs
@@ -2,6 +2,11 @@
 {
     public class Part_manufacturer
     {
-        //This table would establish a many-to-many relationship between the parts and the manufacturers, linking the specific parts with the manufacturer, including the part's warranty information and production date.
+        //This table would establish a many-to-many relationship between the parts and the manufacturers,
+        //linking the specific parts with the manufacturer, including the part's warranty information and production date.
+        public int ManufacturerId { get; set; }
+
+        public string ManufacturerName { get; set; }
+
     }
 }
