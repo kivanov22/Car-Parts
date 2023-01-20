3d0c480ee6c5bdd911f864e96f6087d2cf47c05f
Kristian Ivanov <kriskopk@abv.bg>
2023-01-20 20:27:36 +0200

    Added required to some of the properties and attributes for Foreign key.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Order.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Order.cs
@@ -1,16 +1,16 @@
-﻿namespace Car_Parts_API.Infrastructure.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Parts_API.Infrastructure.Models
 {
     public class Order
     {
 
         //This table would store detailed information about customer orders, including customer information, order date,
         //shipping address, and the specific parts that were ordered along with their quantity.
-
+        [Key]
         public int Id { get; set; }
 
-        public int CustomerId { get; set; }
-
-        public int PartId { get; set; }
 
         public DateTime OrderDate { get; set; }
 
@@ -18,12 +18,22 @@
 
         public decimal TotalCost { get; set; }
 
-        public string PaymentMethod { get; set; }
+        public required string PaymentMethod { get; set; }
 
-        public string ShippingAddress { get; set; }
+        public required string ShippingAddress { get; set; }
 
-        public string Status { get; set; }
+        public required string Status { get; set; }
 
-        public string Notes { get; set; }
+        public string? Notes { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public int CustomerId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        [ForeignKey("PartId")]
+        public int PartId { get; set; }
+
+        public Part Part { get; set; }
     }
 }
