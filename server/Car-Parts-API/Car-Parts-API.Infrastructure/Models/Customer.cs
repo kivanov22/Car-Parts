cf3b858f3ebde70064963b12417cc248e4c2a863
Kristian Ivanov <kriskopk@abv.bg>
2023-01-20 20:24:12 +0200

    Added required to the Name property. Not required to the Address. Required to the Email.Foreign key attribute of OrderId.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Customer.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Customer.cs
@@ -1,25 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Car_Parts_API.Infrastructure.Models
 {
     public class Customer
     {
+        [Key]
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
         public int PhoneNumber { get; set; }
 
-        public string Email { get; set; }
+        public required string Email { get; set; }
 
-        public string Password { get; set; }
+        //public required string Password { get; set; }
 
+        [ForeignKey("OrderId")]
         public int OrderId { get; set; }
 
         public Order Orders { get; set; }
