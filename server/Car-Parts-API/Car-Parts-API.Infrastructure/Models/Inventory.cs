c14b5a017dc324d7163efefdee9ab6cf2181b82f
Kristian Ivanov <kriskopk@abv.bg>
2023-01-20 20:26:23 +0200

    Changed the naming of Id and property to int. Added required to the Location property. Foreign key attribute to PartId.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Inventory.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Inventory.cs
@@ -1,21 +1,25 @@
-﻿namespace Car_Parts_API.Infrastructure.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Car_Parts_API.Infrastructure.Models
 {
     public class Inventory
     {
         //This table would store detailed information about the current inventory of parts, including the
         //quantity on hand, reorder level, and the supplier details.
         //Ask chatgpt Inventory and Vendor
-
-        public int InventoryId { get; set; }
-
-        public int PartId { get; set; }
+        [Key]
+        public int Id { get; set; }
 
         public int Quantity { get; set; }
 
-        public string Location { get; set; }
+        public required string Location { get; set; }
 
         public DateTime DeliveryDate { get; set; }
 
-        public string SupplierDetails { get; set; }
+        public string? SupplierDetails { get; set; }
+
+        [ForeignKey("PartId")]
+        public int PartId { get; set; }
     }
 }
