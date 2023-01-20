9c6bb8b4462f84d3da7bcfc0d3dfa36b1803a574
Kristian Ivanov <kriskopk@abv.bg>
2023-01-20 20:22:05 +0200

    Added a Part and VehicleModel properties.

--- server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Compatibility.cs
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/Compatibility.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Car_Parts_API.Infrastructure.Models
+﻿namespace Car_Parts_API.Infrastructure.Models
 {
     public class Compatibility
     {
         public int PartId { get; set; }
 
+        public Part Part { get; set; }
+
         public int ModelId { get; set; }
+
+        public VehicleModel Vehicle_Model { get; set; }
     }
 }
