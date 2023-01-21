50adbe60e2aaf72c20ee5b998ea1ddd236aff850
Kristian Ivanov <kriskopk@abv.bg>
2023-01-21 15:39:36 +0200

    User model.

---
+++ server/Car-Parts-API/Car-Parts-API.Infrastructure/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Car_Parts_API.Infrastructure.Models
+{
+    public class User
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
