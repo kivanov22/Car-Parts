ae9092742664cf61a3d0ef416655bd9124f69b77
Kristian Ivanov <kriskopk@abv.bg>
2023-02-02 13:54:07 +0200

    Added automapper and configured it.

---
+++ server/CarParts-API/CarParts.API.Core/AutoMapperProfile/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Car_Parts_API.Infrastructure.Data.Models;
+using CarParts.API.Core.ViewModels;
+
+namespace CarParts.API.Core.AutoMapperProfile
+{
+    public class AutoMapperProfile:Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Part, PartDto>();
+            CreateMap<PartDto, Part>();
+            CreateMap<IEnumerable<PartDto>, Part>();
+            CreateMap<IEnumerable<Part>, PartDto>();
+            CreateMap<List<Part>, PartDto>();
+            CreateMap<List<Part>, PartDto>();
+        }
+    }
+}
