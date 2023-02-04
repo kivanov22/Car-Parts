c003a7974a1bd7c4238d37dddebe4181908af5ab
Kristian Ivanov <kriskopk@abv.bg>
2023-02-04 16:17:36 +0200

    Created extension for services.

---
+++ server/CarParts-API/CarParts-API/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using CarParts.API.Core.Interfaces;
+using CarParts.API.Core.Services;
+
+namespace CarParts_API.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IPartService, PartService>();
+
+            return services;
+        }
+
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<CarPartsContext>(options =>
+                options.UseSqlServer(connectionString));
+            //services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+    }
+}
