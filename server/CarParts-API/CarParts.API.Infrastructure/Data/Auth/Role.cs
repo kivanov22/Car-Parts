03acb77722dcab368d21873b4f994661776cdbae
Kristian Ivanov <kriskopk@abv.bg>
2023-02-05 20:25:21 +0200

    Created User,RefreshToken and Role entities. For Jwt authentication.

---
+++ server/CarParts-API/CarParts.API.Infrastructure/Data/Auth/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace CarParts.API.Infrastructure.Data.Auth
+{
+    public enum Role
+    {
+        Admin,
+        User,
+    }
+}
---
+++ server/CarParts-API/CarParts.API.Infrastructure/Data/Auth/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CarParts.API.Infrastructure.Data.Auth
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string PasswordHash { get; set; }
+
+        [JsonIgnore]
+        public List<RefreshToken> RefreshTokens { get; set; }
+    }
+}
