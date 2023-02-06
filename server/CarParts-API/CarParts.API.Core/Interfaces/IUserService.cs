7c3cd870a594b43a25a270d8943184a17a01114a
Kristian Ivanov <kriskopk@abv.bg>
2023-02-06 20:21:01 +0200

    Added logic for user interface.

---
+++ server/CarParts-API/CarParts.API.Core/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using CarParts.API.Core.Auth;
+using CarParts.API.Infrastructure.Data.Auth;
+
+namespace CarParts.API.Core.Interfaces
+{
+    public interface IUserService
+    {
+        AuthenticateResponse Authenticate(AuthenticateRequest model, string ipAddress);
+        AuthenticateResponse RefreshToken(string token, string ipAddress);
+        void RevokeToken(string token, string ipAddress);
+        IEnumerable<User> GetAll();
+        User GetById(int id);
+    }
+}
