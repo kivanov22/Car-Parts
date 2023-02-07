50007ffe4c454ae45dc761a68184793a4fa8d9c6
Kristian Ivanov <kriskopk@abv.bg>
2023-02-07 17:14:30 +0200

    JwtUtils interface with methods for generating token,validating token and generating refresh token.

---
+++ server/CarParts-API/CarParts.API.Core/Auth/JwtUtils/IJwtUtils.cs
@@ -0,0 +1,11 @@
+﻿using CarParts.API.Infrastructure.Data.Auth;
+
+namespace CarParts.API.Core.Auth.JwtUtils
+{
+    public interface IJwtUtils
+    {
+        public string GenerateJwtToken(User user);
+        public int? ValidateJwtToken(string token);
+        public RefreshToken GenerateRefreshToken(string ipAddress);
+    }
+}
