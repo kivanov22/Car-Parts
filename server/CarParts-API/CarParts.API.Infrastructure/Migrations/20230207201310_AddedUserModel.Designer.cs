6b9e8155d19ddbd0948410f1627c2344f06ef666
Kristian Ivanov <kriskopk@abv.bg>
2023-02-09 22:58:41 +0200

    Created new migrations after adding the user entity.

--- server/CarParts-API/CarParts.API.Infrastructure/Migrations/20230207201310_AddedUserModel.Designer.cs
+++ server/CarParts-API/CarParts.API.Infrastructure/Migrations/20230207201310_AddedUserModel.Designer.cs
@@ -4,6 +4,7 @@ using CarParts.API.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarParts.API.Infrastructure.Migrations
 {
     [DbContext(typeof(CarPartsContext))]
-    partial class CarPartsContextModelSnapshot : ModelSnapshot
+    [Migration("20230207201310_AddedUserModel")]
+    partial class AddedUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
