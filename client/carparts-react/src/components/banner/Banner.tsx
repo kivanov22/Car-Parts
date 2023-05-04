da522ac953b7656dce1eebaff0087a48ae7b271e
Kristian Ivanov <kriskopk@abv.bg>
2023-05-04 21:44:49 +0300

    Changed the banner component with search.

--- client/carparts-react/src/components/banner/Banner.tsx
+++ client/carparts-react/src/components/banner/Banner.tsx
@@ -1,63 +1,69 @@
-import React from 'react'
-import { Carousel } from 'react-responsive-carousel';
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./Banner.scss"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import "./Banner.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useSearchParams } from "react-router-dom";
 // import {car1} from "../../../public/images/audi.jpg"
 
-
 const Banner = () => {
-  return (
-    <div className='banner'>
-        <div className='search'>
-        <label htmlFor="make">Make</label>
-        <select name="make" id="make">
-            <option value="opel">Opel</option>
-            <option value="Audi">Audi</option>
-            <option value="BMW">BMW</option>
-            <option value="Mercedes">Mercedes</option>
-        </select>
-        <label htmlFor="model">Model</label>
-        <select name="model" id="model">
-            <option value="opel">Opel</option>
-            <option value="Audi">Audi</option>
-            <option value="BMW">BMW</option>
-            <option value="Mercedes">Mercedes</option>
-        </select>
-        <label htmlFor="specification">Specification</label>
-        <select name="specification" id="specification">
-            <option value="opel">Opel</option>
-            <option value="Audi">Audi</option>
-            <option value="BMW">BMW</option>
-            <option value="Mercedes">Mercedes</option>
-        </select>
-        <button >
-            <FontAwesomeIcon icon={faSearch}/>
-            Search</button>
-        </div>
-        <div className="container">
-            <Carousel
-            autoPlay
-            infiniteLoop
-            showStatus={false}
-            showThumbs={false}
-            interval={5000}
-            >
-                <div>
-                    <img src="./images/audi.jpg" alt="img" />
-                </div>
-                <div>
-                    <img src="./images/mercedes.jpg" alt="img" />
-                </div>
-                <div>
-                    <img src="./images/mercedes2.jpg" alt="img" />
-                </div>
-            </Carousel>
-        </div>
-        
-    </div>
-  )
-}
+  const [search, setSearch] = useSearchParams();
 
-export default Banner
+  const handleSearch = () => {
+    setSearch();
+  };
+
+  return (
+    <div className="banner">
+      <div className="container">
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showThumbs={false}
+          interval={5000}
+        >
+          <div className="darkerImg">
+            <img src="./images/audi.jpg" alt="img" />
+          </div>
+          <div className="darkerImg">
+            <img src="./images/mercedes.jpg" alt="img" />
+          </div>
+          <div className="darkerImg">
+            <img src="./images/mercedes2.jpg" alt="img" />
+          </div>
+        </Carousel>
+        <div className="search">
+          <label htmlFor="make">Make</label>
+          <select name="make" id="make">
+            <option value="opel">Opel</option>
+            <option value="Audi">Audi</option>
+            <option value="BMW">BMW</option>
+            <option value="Mercedes">Mercedes</option>
+          </select>
+          <label htmlFor="model">Model</label>
+          <select name="model" id="model">
+            <option value="opel">Opel</option>
+            <option value="Audi">Audi</option>
+            <option value="BMW">BMW</option>
+            <option value="Mercedes">Mercedes</option>
+          </select>
+          <label htmlFor="specification">Specification</label>
+          <select name="specification" id="specification">
+            <option value="opel">Opel</option>
+            <option value="Audi">Audi</option>
+            <option value="BMW">BMW</option>
+            <option value="Mercedes">Mercedes</option>
+          </select>
+          <button>
+            <FontAwesomeIcon icon={faSearch} />
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
